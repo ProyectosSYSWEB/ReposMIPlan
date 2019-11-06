deae84f04e7880d2ed4c271499b2e1d90fbd2322
Rafael Rosales <rafaeltux@hotmail.com>
2019-11-06 09:17:36 -0600

    ClasesRestantes

---
+++ MIPlan/MIPlan/Models/AreasAtencion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIPlan.Models
+{
+    public class AreasAtencion
+    {
+        public int Id { get; set; }
+        public string Dependencia { get; set; }
+        public string Clave { get; set; }
+        public string Descripcion { get; set; }
+        public string Status { get; set; }
+    }
+}
