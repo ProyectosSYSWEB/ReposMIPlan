508c740e8b335ff16ba3a396559274ff7e05c160
Jose Alberto Andrade Valdovinos <valdo_jose@hotmail.com>
2019-11-08 10:41:57 -0600

    CatBasicos 1er version

---
+++ MIPlan/MIPlan/Scripts/Controllers/CatController.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="../Models/ViaticosModel.js"/>
+/// <reference path="../global.js"/>
+
+
+(function () {
+    var app = angular.module('SUNVAWeb', []);
+
+
+    app.controller('ViaticosController', ['$scope', '$compile', function ($scope, $compile) {
+        var self = this;
+
+
+        this.InicioViaticos = function () {
+        };
+    
+}]);
+
+  
+
+}) ();
+
