3c1176b8091b48a064263c04c67e1068c359a409
Rafael Rosales <rafaeltux@hotmail.com>
2019-11-12 11:13:14 -0600

    Areas Atencion

--- MIPlan/MIPlan/Controllers/CatalogoController.cs
+++ MIPlan/MIPlan/Controllers/CatalogoController.cs
@@ -166,5 +166,18 @@ namespace MIPlan.Controllers
             }
         }
 
+        public JsonResult ObtenerGridAreasAtencion()
+        {
+            try
+            {
+                var List = CursorDataContext.ObtenerAreasAtencion();
+                return Json(List, JsonRequestBehavior.AllowGet);                
+            }
+            catch (Exception ex)
+            {
+                return Json(ex.Message, JsonRequestBehavior.AllowGet);
+            }
+        }
+
     }
 }
