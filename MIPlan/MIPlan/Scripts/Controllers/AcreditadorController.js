bf2b96b0e7bb0e2e2d702dc0ffe9ec14c66e4b4d
Adriàn Cordero <Adrin Cordero@ADRIANCORDERO>
2019-11-25 11:47:23 -0600

    prueba

---
+++ MIPlan/MIPlan/Scripts/Controllers/AcreditadorController.js
@@ -0,0 +1,70 @@
+﻿// <reference path="../Models/AcreditadorModel.js"/>
+
+(function () {
+    var app = angular.module('MIPlanWeb', []);
+
+    app.controller('MIPlanController', ['$scope', '$compile', function ($scope, $compile) {
+        var self = this;
+        //var self = this;
+
+        this.Inicio = function () {
+            CargarCombos();
+            CargarGrid();
+        };
+
+        var CargarCombos = function () {
+            ObtenerDependencias();
+        };
+
+
+        var ObtenerDependencias = function () {
+            catalogoContext.ObtenerDependencias(function (resp) {
+                switch (resp.ressult) {
+                    case "tgp":
+                        self.dependencias = catalogoContext.dependenciaslst;
+                        self.cve_dependencia = catalogoContext.dependenciaslst[0].Id;
+                        break;
+                    case "notgp":
+                        self.mensaje_gral = resp.message;
+                        break;
+                    default:
+                        break;
+                }
+                $scope.$apply();
+            });
+        };
+
+        var CargarGrid = function () {
+            catalogoContext.ObtenerAcreditadores(self.cve_dependencia, function (resp) {
+                switch (resp.ressult) {
+                    case "tgp":
+                        self.acreditador = catalogoContext.acreditadoreslst;
+                        break;
+                    case "notgp":
+                        self.mensaje_gral = resp.message;
+                        break;
+                    default:
+                        break;
+                }
+                $scope.$apply();
+            });
+        };
+
+        this.ValorDependencia = function () {
+            alert(self.cve_dependencia);
+        };
+
+        this.BorrarAcreditador = function (Indice) {
+            alert(Indice);
+        };
+
+
+
+    }]);
+
+
+
+})();
+
+
+
---
+++ MIPlan/MIPlan/Scripts/Models/AcreditadorModel.js
@@ -0,0 +1,57 @@
+﻿var catalogoContext =
+{
+    dependenciaslst: [],
+    acreditadoreslst: [],
+    ObtenerDependencias: function (callBackResult) {
+        var self = this;
+        self.dependenciaslst.length = 0;
+        //var urlServer = "http://localhost:53805/";
+        $.ajax(
+            {
+                type: 'GET',
+                cache: false,
+                url: urlServer + 'Catalogo/ObtenerDependencias',
+                success: function (resp) {
+                    for (var i = 0; i < resp.length; i++) {
+                        self.dependenciaslst.push({ Id: resp[i].Id, Descripcion: resp[i].Descripcion });
+                    }
+                    if (callBackResult != undefined) {
+                        callBackResult({ ressult: 'tgp', message: null });
+                    }
+                },
+                error: function (ex) {
+                    if (callBackResult != undefined) {
+                        callBackResult({ ressult: "notgp", message: "Ocurrio un error al obtener los datos en ObtenerDependencias." });
+                    }
+                }
+            });
+
+    },
+    ObtenerAcreditadores: function (Dependencia, callBackResult) {
+        var self = this;
+        self.Acreditadoreslst.length = 0;
+        $.ajax(
+            {
+                type: 'GET',
+                cache: false,
+                url: urlServer + 'Catalogo/ObtenerGridAcreditadores',
+                data: { Dependencia },
+                success: function (resp) {
+                    for (var i = 0; i < resp.length; i++) {
+                        self.acreditadoreslst.push({ Identificador: resp[i].Id, TipoBasico: resp[i].Tipo, Cve: resp[i].Clave, Estatus: resp[i].Status, Desc: resp[i].Descripcion, Valor: resp[i].Valor, Orden: resp[i].Orden });
+                    }
+                    if (callBackResult !== undefined) {
+                        callBackResult({ ressult: 'tgp', message: null });
+                    }
+                },
+                error: function (ex) {
+                    if (callBackResult !== undefined) {
+                        callBackResult({ ressult: "notgp", message: "Ocurrio un error al obtener los datos en ObtenerDependencias." });
+                    }
+                }
+            });
+
+    },
+
+
+};
