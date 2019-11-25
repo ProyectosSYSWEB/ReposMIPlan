a5f2a947790450e72e067682809216a4c1221146
RBurgos1 <57411760+RBurgos1@users.noreply.github.com>
2019-11-25 10:44:16 -0600

    Cambios

--- MIPlan/MIPlan/Scripts/Controllers/AreasAtencionController.js
+++ MIPlan/MIPlan/Scripts/Controllers/AreasAtencionController.js
@@ -1,1 +1,50 @@
-﻿
+﻿// <reference path="../Models/AreasAtencionModel.js"/>
+
+(function () {
+    var app = angular.module('MIPlanWeb', []);
+
+
+    app.controller('MIPlanController', ['$scope', '$compile', function ($scope, $compile) {
+        var self = this;
+        var abc = "";
+
+        this.Inicio = function () {
+            CargarCombos();
+        };
+
+        var CargarCombos = function () {
+            ObtenerDependencias();
+        };
+
+
+        var ObtenerDependencias = function () {
+            catalogoContext.ObtenerDependencias(function (resp) {
+                switch (resp.ressult) {
+                    case "tgp":
+                        self.dependencias = catalogoContext.dependenciaslst;
+                        break;
+                    case "notgp":
+                        self.mensaje_gral = resp.message;
+                        break;
+                    default:
+                        break;
+                }
+                $scope.$apply();
+            });
+        };
+
+        this.ValorDependencia = function () {
+            alert(self.cve_dependencia);
+        };
+
+        this.BorrarBasico = function (Indice) {
+            alert(Indice);
+        };
+
+
+
+    }]);
+
+
+
+})();
