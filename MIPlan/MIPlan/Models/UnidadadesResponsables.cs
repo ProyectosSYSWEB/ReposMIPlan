1bb03b901c9db28a703c8566ead32f46267f39ad
Luis LG <LuisLSC@outlook.com>
2019-11-25 14:13:43 -0600

    Formulario Unidades Responables

---
+++ MIPlan/MIPlan/Models/UnidadadesResponsables.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIPlan.Models
+{
+    public class UnidadadesResponsables
+    {
+        public int Id { get; set; }
+        public string Dependecia { get; set; }
+        public string Clave { get; set; }
+        public string Descripcion { get; set; }
+        public string Status { get; set; }
+
+    }
+}
---
+++ MIPlan/MIPlan/Scripts/Controllers/UnidadesResponsablesController.js
@@ -0,0 +1,69 @@
+﻿
+// <reference path="../Models/CatModel.js"/>
+
+(function () {
+    var app = angular.module('MIPlanWeb', []);
+
+    app.controller('MIPlanController', ['$scope', '$compile', function ($scope, $compile) {
+        var self = this;
+        
+
+        this.Inicio = function () {
+            CargarCombos();
+            CargarGrid();
+        };
+
+        var CargarCombos = function () {
+            ObtenerDependencias();
+        };
+
+
+        var ObtenerDependencias = function () {
+            catalogoContext.ObtenerDependencias(function (resp) {
+                switch (resp.ressult) {
+                    case "tgp":
+                        self.dependencias = catalogoContext.dependenciaslst;
+                        self.cve_dependencia = catalogoContext.dependenciaslst[0].Id;
+                        break;
+                    case "notgp":
+                        self.mensaje_gral = resp.message;
+                        break;
+                    default:
+                        break;
+                }
+                $scope.$apply();
+            });
+        };
+
+        var CargarGrid = function () {
+            catalogoContext.ObtenerUnidades(self.cve_dependencia, function (resp) {
+                switch (resp.ressult) {
+                    case "tgp":
+                        self.unidades = catalogoContext.unidadesRLst;
+                        break;
+                    case "notgp":
+                        self.mensaje_gral = resp.message;
+                        break;
+                    default:
+                        break;
+                }
+                $scope.$apply();
+            });
+        };
+
+        this.ValorDependencia = function () {
+            alert(self.cve_dependencia);
+        };
+
+        this.BorrarBasico = function (Indice) {
+            alert(Indice);
+        };
+
+
+
+    }]);
+
+
+
+})();
+
